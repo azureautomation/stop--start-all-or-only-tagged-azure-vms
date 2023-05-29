758567b1f3adf1c730a0d59eaeb479da8a7ec3ae
BHAWNA RAWAT <bhawna121@users.noreply.github.com>
2023-05-29 23:02:48 +0530

    Update StopStartVMs.ps1

--- StopStartVMs.ps1
+++ StopStartVMs.ps1
@@ -20,29 +20,20 @@ try
     # Ensures you do not inherit an AzContext in your runbook
     $null = Disable-AzContextAutosave –Scope Process
 
-    $Conn = Get-AutomationConnection -Name AzureRunAsConnection
-    
-    $null = Connect-AzAccount `
-                    -ServicePrincipal `
-                    -Tenant $Conn.TenantID `
-                    -ApplicationId $Conn.ApplicationID `
-                    -CertificateThumbprint $Conn.CertificateThumbprint
+	$null= Connect-AzAccount -Identity
+   
 
     Write-Output "Successfully logged into Azure." 
 } 
 catch
 {
-    if (!$Conn)
-    {
-        $ErrorMessage = "Service principal not found."
-        throw $ErrorMessage
-    } 
-    else
-    {
+    
         Write-Error -Message $_.Exception
         throw $_.Exception
-    }
+    
 }
+
+
 ## End of authentication
 
 ## Getting all virtual machines
